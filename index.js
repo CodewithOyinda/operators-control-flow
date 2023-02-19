76b6f21d379a0c4a9fec0de12a49b533cae81e1e
CodewithOyinda <honeybee08186@gmail.com>
2023-02-19 22:57:04 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// Question 3
+let scienceSubjects = "Physics, Chemistry, Biology, Technical Drawing "
+let socialScienceSubjects = "Accounting, Commerce, Marketing, Geography "
+let artsSubjects = "Government, Economics, Literature, History "
+let generalSubjects = "English, Mathematics "
+let classGroup = artsSubjects
+
+if (classGroup == artsSubjects){
+    console.log (artsSubjects + generalSubjects)
+}
+else if (classGroup == artsSubjects){
+    console.log (scienceSubjects + generalSubjects)
+}
+// Question 4
+for (i = 1; i < 20; i +=7){
+console.log(i)
+}
+// Question 5
+let num = 12;
+2 ** 2;
+2 ** 3;
+2 ** 4;
+
+let pwr = 2 ** 3;
+console.log ("The number " + pwr + " is the power of 2 nearest to " + num)
+
+  
+  
---
+++ readme.md
@@ -0,0 +1,32 @@
+
+## Arithmetic operators
++, -, *, /
+Example: Let a = 2;
+         Let b = 5;
+
+         console.log(a+b)
+         console.log(a*b)
+
+## Assignment Operators
+ =, +=, -=, /=, *=
+ Example: Let a = 5
+          Let b = a + 2
+
+## Comparison Operators
+<, >, <=, >=, !+, ==, ===
+Example: Let x = 5;
+        console.log(x > 6)
+        console.log(x <= 5)
+
+## Logical Operators
+&&, ||, !
+Example: let highIncome = true;
+         let goodCreditScore = true;
+         let eligibleForLoan = highIncome && goodCreditScore;
+         console.log(eligibleForLoan) // true
+
+
+## Bitwise Operators
+&, |, ^, ~, <<, >>, >>>
+Example: Let x = 5 ^ 1;
+        
